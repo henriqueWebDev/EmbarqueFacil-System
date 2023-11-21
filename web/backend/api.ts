9301a5eae8d8e744ca3537e903f3af26dfc996b8
Guilherme de Souza Fornaciari <FornaciariWebDev@gmail.com>
2023-11-21 02:09:21 -0400

    Enabled Cors

--- web/backend/api.ts
+++ web/backend/api.ts
@@ -3,6 +3,7 @@ import { AppModule } from './src/infra/http/nest/app.module';
 
 async function main(port: number) {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors()
   await app.listen(port);
 }
 const port = 3000;
