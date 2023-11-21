912c0702c875836fbeb65b8488eecc42d74bf1fc
Guilherme de Souza Fornaciari <FornaciariWebDev@gmail.com>
2023-11-20 23:50:24 -0400

    connection string added

---
+++ web/backend/src/infra/http/nest/mongodbProvider.ts
@@ -0,0 +1,8 @@
+import * as mongoose from 'mongoose';
+import { config } from 'dotenv';
+config();
+export const databaseProviders = {
+  provide: 'DATABASE_CONNECTION',
+  useFactory: (): Promise<typeof mongoose> =>
+    mongoose.connect(process.env.CONNECTIONSTRING),
+};
